a90eb1b05eb1c2362367bb759e484c9ed1113a56
tochikat <eitochilkina@edu.hse.ru>
2025-04-28 12:32:59 +0300

    may be last version 3

--- collect_files.sh
+++ collect_files.sh
@@ -4,13 +4,13 @@ INPUT=$1
 OUTPUT=$2
 MAX_DEPTH=0
 
-while [ -n "$1" ]
-do
-case "$1" in
---max_depth) MAX_DEPTH=$2 ;;
-esac
-shift
-done
+#while [ -n "$1" ]
+#do
+#case "$1" in
+#--max_depth) MAX_DEPTH=$2 ;;
+#esac
+#shift
+#done
 
 RESULT=$(python3 script.py $INPUT $OUTPUT $MAX_DEPTH 2>&1)
 # echo -e $RESULT
