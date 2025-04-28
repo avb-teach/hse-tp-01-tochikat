b88ac8a42321f9c9f8e7492993336c49ae012876
tochikat <eitochilkina@edu.hse.ru>
2025-04-28 12:44:36 +0300

    may be last version 5

--- script.py
+++ script.py
@@ -6,12 +6,13 @@ OUTPUT = sys.argv[2]
 MAX_DEPTH = int(sys.argv[3])
 # print(INPUT, OUTPUT, MAX_DEPTH)
 for root, dirs, files in os.walk(INPUT):
+    root = root.replace(INPUT, "")
     path = root.split(os.sep)
-    cp = "/".join(path[:])
+    cp = "/".join(path)
     for file in files:
-        # if len(path) - 1 <= MAX_DEPTH:
-        #     if not os.path.isdir(f'{OUTPUT}/{cp}'):
-        #         os.system(f"mkdir {OUTPUT}/{cp}")
-        #     os.system(f"cp {INPUT}/{cp}/{file} {OUTPUT}/{cp}/")
-        # else:
-            os.system(f"cp {cp}/{file} {OUTPUT}/")
+        if len(path) <= MAX_DEPTH:
+            if not os.path.isdir(f'{OUTPUT}/{cp}'):
+                os.system(f"mkdir {OUTPUT}/{cp}")
+            os.system(f"cp {INPUT}/{cp}/{file} {OUTPUT}/{cp}/")
+        else:
+            os.system(f"cp {INPUT}/{cp}/{file} {OUTPUT}/")
