bbbdce07ce5fccaddf1c79e3f6c766ae8addd30c
tochikat <eitochilkina@edu.hse.ru>
2025-04-28 12:15:59 +0300

    may be last version

--- collect_files.sh
+++ collect_files.sh
@@ -2,6 +2,15 @@
 
 INPUT=$1
 OUTPUT=$2
+MAX_DEPTH=0
 
-RESULT=$(python3 script.py $INPUT $OUTPUT 2>&1)
+while [ -n "$1" ]
+do
+case "$1" in
+--max_depth) MAX_DEPTH=$2 ;;
+esac
+shift
+done
+
+RESULT=$(python3 script.py $INPUT $OUTPUT $MAX_DEPTH 2>&1)
 # echo -e $RESULT
--- script.py
+++ script.py
@@ -3,9 +3,15 @@ import sys
 
 INPUT = sys.argv[1]
 OUTPUT = sys.argv[2]
-
+MAX_DEPTH = int(sys.argv[3])
+# print(INPUT, OUTPUT, MAX_DEPTH)
 for root, dirs, files in os.walk(INPUT):
     path = root.split(os.sep)
-    cp = "/".join(path)
+    cp = "/".join(path[1:])
     for file in files:
-        os.system(f"cp {cp}/{file} {OUTPUT}/")
+        if len(path) - 1 <= MAX_DEPTH:
+            if not os.path.isdir(f'{OUTPUT}/{cp}'):
+                os.system(f"mkdir {OUTPUT}/{cp}")
+            os.system(f"cp {INPUT}/{cp}/{file} {OUTPUT}/{cp}/{file}")
+        else:
+            os.system(f"cp {INPUT}/{cp}/{file} {OUTPUT}/")
