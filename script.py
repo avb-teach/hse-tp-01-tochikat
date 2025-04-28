37fd995825d3c49b5f48843f7f076bdc0cd0e4d0
tochikat <eitochilkina@edu.hse.ru>
2025-04-28 12:35:17 +0300

    may be last version 4

--- script.py
+++ script.py
@@ -7,11 +7,11 @@ MAX_DEPTH = int(sys.argv[3])
 # print(INPUT, OUTPUT, MAX_DEPTH)
 for root, dirs, files in os.walk(INPUT):
     path = root.split(os.sep)
-    cp = "/".join(path[1:])
+    cp = "/".join(path[:])
     for file in files:
         # if len(path) - 1 <= MAX_DEPTH:
         #     if not os.path.isdir(f'{OUTPUT}/{cp}'):
         #         os.system(f"mkdir {OUTPUT}/{cp}")
         #     os.system(f"cp {INPUT}/{cp}/{file} {OUTPUT}/{cp}/")
         # else:
-            os.system(f"cp {INPUT}/{cp}/{file} {OUTPUT}/")
+            os.system(f"cp {cp}/{file} {OUTPUT}/")
